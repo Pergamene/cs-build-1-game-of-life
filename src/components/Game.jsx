8c702bf610d9da42688d40c21748cd820cb071a2
Pergamene <smartz373@gmail.com>
2020-07-26 10:13:31 -0600

    init

---
+++ src/components/Game.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+
+  },
+});
+
+const Game = () => {
+  const classes = useStyles();
+
+  return (
+    <div></div>
+  );
+};
+
+export default Game;
